6e285ae7797c8caa1f17a0b3975d1f7bae48a6b8
Daler Firuzov <110167520+dalercreates@users.noreply.github.com>
2023-04-15 17:16:50 +0500

    the main page is ready and minor bug have fixed

--- js/script.js
+++ js/script.js
@@ -4,10 +4,13 @@ const dropdownButton = document.getElementById('dropdown-btn');
 const toggleBtn = document.getElementById('toggle-menu');
 const mobileNav = document.getElementById('mobile__nav');
 
+let scrolled = false
 document.addEventListener('scroll', () => {
     if (scrollY > 0) {
+        scrolled = true
         nav.classList.add('sticky');
     } else {
+        scrolled = false
         nav.classList.remove('sticky');
     }
 });
@@ -27,10 +30,56 @@ toggleBtn.addEventListener('click', () => {
     if (toggleBtn.classList.contains('checked')) {
         toggleBtn.classList.remove('checked');
         mobileNav.classList.remove('active');
-        nav.classList.remove('sticky');
+        if (!scrolled) {
+            nav.classList.remove('sticky');
+        }
     } else {
         toggleBtn.classList.add('checked');
         mobileNav.classList.add('active');
         nav.classList.add('sticky');
     }
+});
+
+// popup
+const openPopups = document.querySelectorAll(".open__popup");
+const closePopups = document.querySelectorAll(".popup__close");
+const popup = document.getElementById("popup");
+
+for (const openPopup of openPopups) {
+    openPopup.addEventListener("click", function (e) {
+        e.preventDefault();
+
+        if (popup !== null) {
+            popup.classList.add("active");
+        }
+    });
+}
+
+for (const closePopup of closePopups) {
+    closePopup.addEventListener("click", () => {
+        if (popup !== null) {
+            popup.classList.remove("active");
+        }
+    });
+}
+
+// swiper JS
+new Swiper(".mySwiper", {
+    cssMode: true,
+    loop: true,
+    centeredSlides: true,
+    autoplay: {
+        delay: 10000,
+        disableOnInteraction: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    mousewheel: true,
+    keyboard: true,
 });
